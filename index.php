3831349d0d98779006388cdd528d2789c8f9805f
Monkey <monkey@dev.local>
2020-08-15 15:20:13 -0700

    Adds place holder screen

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://fonts.googleapis.com/css?family=Bungee+Hairline" rel="stylesheet"></link>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <style type="text/css"><!--
+
+  body {
+    position: absolute;
+    overflow:hidden;
+    font-family: "Bungee Hairline";
+    line-height: 26px;
+    background-color: #000000;
+    margin-top: 35vh;
+    margin-left: 44vw;
+  }
+
+  ::-webkit-input-placeholder {
+    color: #84c754;
+    font-family: "Bungee Hairline";
+  }
+
+  ::-moz-placeholder {
+    color: #84c754;
+    font-family: "Bungee Hairline";
+  }
+
+  :-ms-input-placeholder {
+    color: #84c754;
+    font-family: "Bungee Hairline";
+  }
+
+  input:-moz-placeholder {
+    color: #84c754;
+    font-family: "Bungee Hairline";
+  }
+
+
+
+
+
+
+  .welcome {
+    position: static;
+    -webkit-animation-play-state: paused;
+    /* Chrome, Safari, Opera */
+    animation-play-state: paused;
+    font-size: 20px;
+    color: #84c754;
+    margin-top: 0;
+    margin-left: -130px;
+    animation-timing-function: ease-in-out;
+    animation-name: moving;
+    animation-duration: 2s;
+    animation-fill-mode: forwards;
+  }
+
+  --></style>
+</head>
+<body onload="">
+
+
+<div class="run">
+  <p class="welcome">WEBSHARESERVER: Your IP is not recognized for login.</p>
+
+
+</div>
+
+
+
+</body>
+</html>
+
