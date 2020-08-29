6fb8208118c928ba090b0155f1998428e4d66e8f
Monkey <monkey@dev.local>
2020-08-29 11:38:41 -0700

    New stat test

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello back to you!\n');
+   response.end('Hello back to you!!!\n');
 }).listen(8080);
 console.log('Server running at http://127.0.0.1:8080/');
