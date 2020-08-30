36f6d9b9a1f49af1de69d0fd93956bd96b23f3d2
Monkey <monkey@dev.local>
2020-08-29 18:49:50 -0700

    Adds gRPC test

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 =========================
 -------------------------
 This project is just a fun place for me to publish stuff
+```mermaid
+gantt
+ title Project Overview
+    dateFormat  MM-YY
+    section OPS
+    Research & requirements :done, a1, 08-20, 30d
+    Review & documentation : after a1, 10d
+    section DEV
+    Implementation      :crit, active, 10-20  , 20d
+    Testing      :crit, 20d
+```
--- app/app.js
+++ app/app.js
@@ -1,7 +1,41 @@
 #!/usr/bin/env nodejs
 var http = require('http');
+var grpc = require('grpc')
+
+var greets = require('./protos/greet_pb')
+var service = require('./protos/greet_grpc_pb')
+
+/* Implements the Greet RPC Service */
+function greet (call, callback) {
+
+    var greeting = new greets.GreetResponse()
+
+    greeting.setResult(
+        "Hello " + call.request.getGreeting().getFirstName() + 
+        " " + call.request.getGreeting().getLastName()
+    );
+
+    callback(null, greeting);
+}
+
+
+
+function main(){
+
+    var server = new grpc.Server()
+    
+    server.addService(service.GreetServiceService, {greet: greet})    
+    server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+    server.start()
+
+    console.log("gRPC server started on 127.0.0.1:50051")
+}
+
+main();
+
+
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello back to you!!!\n');
+   response.end('Hello back to you--!\n');
 }).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Web Server running at http://127.0.0.1:8080/');
