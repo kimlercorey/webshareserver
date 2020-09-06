ec605412e94ce6d99a5eacbcead4912351ef2f6f
Monkey <monkey@dev.local>
2020-09-06 12:21:14 -0700

    Use certificates for communication

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ var grpc = require('grpc')
 var greets = require('./protos/greet_pb')
 var service = require('./protos/greet_grpc_pb')
 
+const fs = require('fs')
+
+
+
 /* Implements the Greet RPC Service */
 function greet (call, callback) {
 
@@ -24,11 +28,21 @@ function main(){
 
     var server = new grpc.Server()
     
+    let credentials_safe = grpc.ServerCredentials.createSsl(
+        fs.readFileSync('../certs/ca.crt'),
+        [{
+            cert_chain: fs.readFileSync("../certs/server.crt"),
+            private_key: fs.readFileSync("../certs/server.key")
+        }], true
+    )
+
+    let credentials_unsafe = grpc.ServerCredentials.createInsecure();
+
     server.addService(service.GreetServiceService, {greet: greet})    
-    server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure());
+    server.bind("service.webshareserver.com:50051", credentials_safe);
     server.start()
 
-    console.log("gRPC app server started on 0.0.0.0:50051");
+    console.log("gRPC app server started on service.webshareserver.com:50051");
 }
 
 main();
